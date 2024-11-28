061876eb15cc718afc83795b39e3f8ff21d3bf24
Agung Suryo <agungsuryo645@gmail.com>
2024-11-29 00:03:54 +0800

    add footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,7 @@
   <footer
     class="flex flex-row justify-center items-end bg-slate-700 min-h-72 text-white"
   >
-    <p class="my-8 w-fit h-fit font-bold text-xl">Footer</p>
+    <p class="my-8 w-fit h-fit font-bold text-xl">&copy; 2024 Agung. All rights reserved.</p>
   </footer>
 </template>
 
